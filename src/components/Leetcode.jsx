d04440255e15c10de61dd74a04bcf6b160e0e17c
Thirumalai <thiru@thiru.com>
2024-09-24 01:05:03 +0530

    mobile responsive

--- src/components/Leetcode.jsx
+++ src/components/Leetcode.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import leetcode from '../images/leet.png'
-import github from '../images/git.png'
-import codechef from '../images/codechef.png'
-import codeforces from '../images/codeforces.png'
-import geeksforgeeks from '../images/geeksforgeeks.png'
-import Atcoders from '../images/atcoders.png'
+import React from 'react';
+import leetcode from '../images/leet.png';
+import github from '../images/git.png';
+import codechef from '../images/codechef.png';
+import codeforces from '../images/codeforces.png';
+import geeksforgeeks from '../images/geeksforgeeks.png';
+import Atcoders from '../images/atcoders.png';
+
 const Leetcode = () => {
   return (
     <div className='updated-big'>
-    <h2>Consistency</h2>
-    <div className='big'>
-    <div className='leet-container'>
-      <div className='leet'><img src={leetcode}/></div>
-      <div className='git'><img src={github} /></div>
-      <div className='codechef'><img src={codechef}/></div>
-      <div className='codeforces'><img src={codeforces}/></div>
-      <div className='geeksforgeeks'><img src={geeksforgeeks}/></div>
-      <div className='atcoders'><img src={Atcoders}/></div>
-
+      <h2>Consistency</h2>
+      <div className='leet-container'>
+        <div className='leet'><img src={leetcode} alt="LeetCode" /></div>
+        <div className='git'><img src={github} alt="GitHub" /></div>
+        <div className='codechef'><img src={codechef} alt="CodeChef" /></div>
+        <div className='codeforces'><img src={codeforces} alt="Codeforces" /></div>
+        <div className='geeksforgeeks'><img src={geeksforgeeks} alt="GeeksforGeeks" /></div>
+        <div className='atcoders'><img src={Atcoders} alt="Atcoders" /></div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Leetcode
+export default Leetcode;
